07648c287cbd5e076d702a903124ad6c53b69ae0
ariefdm19 <ariefdmofficial0@gmail.com>
2021-12-21 20:47:54 +0700

    membuat controller user di folder admin

---
+++ app/Controllers/Admin/Users.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers\Admin;
+use App\Controllers\BaseController;
+
+class Users extends BaseController
+{
+    public function index()
+    {
+        echo 'Ini Controller Users Method Index yang ada di dalam folder admin';
+    }
+
+
+}
