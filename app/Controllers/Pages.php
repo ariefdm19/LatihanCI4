577978c75d8636c0c03bd0a19f770b733d717799
ariefdm19 <ariefdmofficial0@gmail.com>
2021-12-22 10:01:36 +0700

    Menambahkan Controller Pages.php

---
+++ app/Controllers/Pages.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controllers;
+
+class Pages extends BaseController
+{
+    public function index()
+    {
+        return view('home');
+    }
+
+    public function about()
+    {
+        return view('about');
+    }
+    
+
+
+}
