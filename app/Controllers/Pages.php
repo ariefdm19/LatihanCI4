ac889079ea1f599d433f5d179189fd936ea67d8e
ariefdm19 <ariefdmofficial0@gmail.com>
2021-12-22 10:35:52 +0700

    Modifikasi Controller Pages.php

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -6,14 +6,20 @@ class Pages extends BaseController
 {
     public function index()
     {
-        echo view('layout/header');
+        $data = [
+            'title' => 'Home | Web ADM'
+        ];
+        echo view('layout/header', $data);
         echo view('pages/home');
         echo view('layout/footer');
     }
 
     public function about()
     {
-        echo view('layout/header');
+        $data = [
+            'title' => 'About | Web ADM'
+        ];
+        echo view('layout/header', $data);
         echo view('pages/about');
         echo view('layout/footer');
     }
