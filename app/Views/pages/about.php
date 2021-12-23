291333f509cf92f5b96efbd692b02298134b4e0f
ariefdm19 <ariefdmofficial0@gmail.com>
2021-12-23 11:18:07 +0700

    mengembalikan file

---
+++ app/Views/pages/about.php
@@ -0,0 +1,16 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+        <h1>About Me</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores nemo optio voluptate rem ipsa doloribus animi debitis autem minima. Eius nisi vero recusandae. Culpa eveniet commodi animi, temporibus quibusdam minus?</p>
+
+
+        </div>
+    </div>
+</div>
+    
+<?= $this->endSection(); ?>
