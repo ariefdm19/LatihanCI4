539e67bd6e020ff8b81741dfede0cf2f6270efdb
ariefdm19 <ariefdmofficial0@gmail.com>
2021-12-23 11:40:14 +0700

    Merapihkan template.php

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -13,22 +13,7 @@
 
   
   <body>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container">
-  <a class="navbar-brand" href="#">Arief Dwi Muhidin</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <a class="nav-link active" href="<?= base_url('/');?>">Home</a>
-      <a class="nav-link" href="<?= base_url('/pages/about');?>">About</a>
-      <a class="nav-link" href="<?= base_url('/');?>">Contact</a>
-      
-    </div>
-  </div>
-  </div>
-</nav>
+ <?= $this->include('layout/navbar'); ?>
 
 <?= $this->renderSection('content'); ?>
 
