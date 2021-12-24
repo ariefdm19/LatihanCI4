d8f95881b72a426e785bc5c6dc225461e514c781
ariefdm19 <ariefdmofficial0@gmail.com>
2021-12-24 08:47:30 +0700

    Menambahkan navbar Komik

--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -9,8 +9,9 @@
       <a class="nav-link active" href="<?= base_url('/');?>">Home</a>
       <a class="nav-link" href="<?= base_url('/pages/about');?>">About</a>
       <a class="nav-link" href="<?= base_url('/pages/contact');?>">Contact</a>
+      <a class="nav-link" href="<?= base_url('/komik/');?>">Komik</a>
       
     </div>
   </div>
   </div>
-</nav>
+</nav> 
