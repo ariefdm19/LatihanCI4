5e4787a408fb612b99700afe2559ff48a0a25932
ariefdm19 <ariefdmofficial0@gmail.com>
2021-12-25 09:59:24 +0700

    modifikasi card pada views detail

--- app/Views/komik/detail.php
+++ app/Views/komik/detail.php
@@ -4,6 +4,7 @@
 <div class="container">
     <div class="row">
         <div class="col">
+        <h2 class="mt-2">Detail Komik</h2>
         <div class="card mb-3" style="max-width: 540px;">
   <div class="row no-gutters">
     <div class="col-md-4">
@@ -11,9 +12,10 @@
     </div>
     <div class="col-md-8">
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          
+        <h5 class="card-title"><?= $komik['judul']; ?></h5>
+        <p class="card-text"><b>Penulis : </b><?= $komik['penulis']; ?></p>
+        <p class="card-text"><small class="text-muted"><b>Penerbit : </b><?= $komik['penerbit']; ?></small></p>
       </div>
     </div>
   </div>
