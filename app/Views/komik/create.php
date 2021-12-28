18045a4f90c990c6feed3c3677c0f3641486ad51
ariefdm19 <ariefdmofficial0@gmail.com>
2021-12-28 19:49:52 +0700

    modifikasi views komik.php / add button tambah

--- app/Views/komik/create.php
+++ app/Views/komik/create.php
@@ -4,7 +4,41 @@
 <div class="container">
     <div class="row">
         <div class="col-8">
-            <h2>Form Tambah Data Komik</h2>
+            <h2 class="my-3">Form Tambah Data Komik</h2>
+            <form action="/komik/save" method="post">
+            <input type="hidden" name="<?= csrf_token() ?>" value="<?= csrf_hash() ?>">
+  <div class="form-group row">
+    <label for="judul" class="col-sm-2 col-form-label">Judul</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="judul" name="judul" autofocus>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="penulis" class="col-sm-2 col-form-label">Penulis</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="penulis" name="penulis">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="penerbit" class="col-sm-2 col-form-label">Penerbit</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="penerbit" name="penerbit">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="sampul" class="col-sm-2 col-form-label">Sampul</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="sampul" name="sampul">
+    </div>
+  </div>
+  
+ 
+  <div class="form-group row">
+    <div class="col-sm-10">
+      <button type="submit" class="btn btn-primary">Tambah Data</button>
+    </div>
+  </div>
+</form>
         </div>
     </div>
 </div>
