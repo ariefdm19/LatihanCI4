8a6def74130b4961797ff44168ae3b12f5782584
ariefdm19 <ariefdmofficial0@gmail.com>
2021-12-28 08:21:01 +0700

    membuat views create.php

---
+++ app/Views/komik/create.php
@@ -0,0 +1,11 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col-8">
+            <h2>Form Tambah Data Komik</h2>
+        </div>
+    </div>
+</div>
+<?= $this->endSection(); ?>
