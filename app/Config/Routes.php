bd7cf7e27b5a1b44742bae39e60c163910aef462
ariefdm19 <ariefdmofficial0@gmail.com>
2021-12-30 07:46:10 +0700

    Done Error routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -33,8 +33,9 @@ $routes->setAutoRoute(true);
 // route since we don't have to scan directories.
 $routes->get('/', 'Pages::index'); 
 $routes->get('/komik/create', 'Komik::create');
-$routes->get('komik/save', 'Komik::save');
-$routes->delete('/komik/(:num)', 'Komik::delete/$id');
+
+$routes->delete('/komik/(:num)', 'Komik::delete/$1');
+
 $routes->get('/komik/(:any)', 'Komik::detail/$1');
 
 
