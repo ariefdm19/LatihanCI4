5743824907a5fa92a594a0a852761ed8172fb351
ariefdm19 <ariefdmofficial0@gmail.com>
2021-12-30 07:31:52 +0700

    membuat button hidden

--- app/Views/komik/detail.php
+++ app/Views/komik/detail.php
@@ -18,7 +18,14 @@
         <p class="card-text"><small class="text-muted"><b> Penerbit : </b><?= $komik['penerbit']; ?></small></p>
       
     <a href="" class="btn btn-warning">Edit</a>
-    <a href="" class="btn btn-danger">Hapus</a>
+
+
+    <form action="/komik/<?= $komik['id']; ?>" method="post" class="d-inline">
+    <?= csrf_field(); ?>
+    <input type="hidden" name="_method" value="DELETE">
+  <button type="submit" class="btn btn-danger">Delete</button>
+  </form>
+
     <br><br>
     <a href="/komik" >Kembali ke daftar komik</a>
     </div>
