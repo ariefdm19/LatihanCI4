06e7f594abb3eccceeaf120ac248160f244de21b
ariefdm19 <ariefdmofficial0@gmail.com>
2021-12-30 08:04:15 +0700

    membuat javascript confirm

--- app/Views/komik/detail.php
+++ app/Views/komik/detail.php
@@ -17,13 +17,13 @@
         <p class="card-text"><b>Penulis : </b><?= $komik['penulis']; ?></p>
         <p class="card-text"><small class="text-muted"><b> Penerbit : </b><?= $komik['penerbit']; ?></small></p>
       
-    <a href="" class="btn btn-warning">Edit</a>
+    <a href="/komik/edit/<?= $komik['slug']; ?>" class="btn btn-warning">Edit</a>
 
 
     <form action="/komik/<?= $komik['id']; ?>" method="post" class="d-inline">
     <?= csrf_field(); ?>
     <input type="hidden" name="_method" value="DELETE">
-  <button type="submit" class="btn btn-danger">Delete</button>
+  <button type="submit" class="btn btn-danger" onclick="return confirm('apakah anda yakin?');">Delete</button>
   </form>
 
     <br><br>
