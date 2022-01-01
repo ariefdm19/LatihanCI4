e3fc2469009037c071b58595a28c04cb6b4045cc
ariefdm19 <ariefdmofficial0@gmail.com>
2022-01-01 19:34:05 +0700

    modifikasi create

--- app/Views/komik/create.php
+++ app/Views/komik/create.php
@@ -31,9 +31,13 @@
         </div>
         <div class="form-group row">
           <label for="sampul" class="col-sm-2 col-form-label">Sampul</label>
+          <!-- <div class="col-sm-2"> -->
+          <!-- <img src="/img/default.png" class="img-thumbnail img-preview"> -->
+          <!-- </div> -->
           <div class="col-sm-10">
-            <input type="file" class="form-control <?= ($validation->hasError('sampul')) ? 'is-invalid' : ''; ?>" id="sampul" name="sampul" autofocus value="<?= old('sampul'); ?>" id="sampul" name="sampul" value="<?= old('sampul'); ?>">
-            <div id="validationServer03Feedback" class="invalid-feedback">
+            <input type="file" class="form-control <?= ($validation->hasError('sampul')) ? 'is-invalid' : ''; ?>" id="sampul" name="sampul" autofocus value="<?= old('sampul'); ?>" id="sampul" name="sampul" onchange="previewImg()">
+
+            <div class="invalid-feedback">
               <?= $validation->getError('sampul'); ?>
             </div>
           </div>
