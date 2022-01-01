b7bbcdcc00143d9f0a0bb9edbda05dc04f92628b
ariefdm19 <ariefdmofficial0@gmail.com>
2022-01-01 17:41:25 +0700

    add validation

--- app/Views/komik/create.php
+++ app/Views/komik/create.php
@@ -32,7 +32,10 @@
         <div class="form-group row">
           <label for="sampul" class="col-sm-2 col-form-label">Sampul</label>
           <div class="col-sm-10">
-            <input type="file" class="form-control" id="sampul" name="sampul" value="<?= old('sampul'); ?>">
+            <input type="file" class="form-control <?= ($validation->hasError('sampul')) ? 'is-invalid' : ''; ?>" id="sampul" name="sampul" autofocus value="<?= old('sampul'); ?>" id="sampul" name="sampul" value="<?= old('sampul'); ?>">
+            <div id="validationServer03Feedback" class="invalid-feedback">
+              <?= $validation->getError('sampul'); ?>
+            </div>
           </div>
         </div>
 
