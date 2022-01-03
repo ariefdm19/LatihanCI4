487ba7c9b478e4759cbf2b2e2128c699b2bf4936
ariefdm19 <ariefdmofficial0@gmail.com>
2022-01-03 10:31:20 +0700

    modift about

--- app/Views/pages/about.php
+++ app/Views/pages/about.php
@@ -7,7 +7,7 @@
         <div class="col">
             <h1>About Me</h1>
             <p>My Github : https://github.com/ariefdm19</p>
-            <p>Ini adalah contoh aplikasi yang dibuat untuk belajar/latihan saya. silakan bebas untuk di modifikasi</p>
+            <p>Ini adalah contoh aplikasi yang dibuat untuk belajar/latihan saya. silakan bebas untuk di modifikasi aplikasi ini</p>
 
 
         </div>
